5056c29aa97dfafa4a3f0a1e67395b6f7cfeb8a6
Thabu <thabu@Thabus-Mac.local>
2018-02-16 21:46:10 +0530

    Initial Commit

---
+++ samplegit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  samplegit
+//
+//  Created by Thabu on 16/02/18.
+//  Copyright © 2018 gopinath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ samplegit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  samplegit
+//
+//  Created by Thabu on 16/02/18.
+//  Copyright © 2018 gopinath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
